b398e91aa0e6556b89142adffeb8be6945503da5
Damien Rajon <145502+pyrho@users.noreply.github.com>
2019-07-03 13:19:03 +0200

    small adjustments & other caps

--- kailh-caps/src/kailh_caps/core.clj
+++ kailh-caps/src/kailh_caps/core.clj
@@ -9,7 +9,7 @@
 
 (def prong-dimensions
   "The x y z dimensions of one prong that go into the stem"
-  [1.2 3 3])
+  [1.2 2.9 3])
 
 (def distance-between-prong-centers
   "The x distance between the middle of each prongs"
@@ -39,19 +39,19 @@
 
 (defmacro def-cap-size
   "Create all the bindings for cap sizes for a given Unit"
-  [unit]
+  [unit name]
   `(do
-     (def ~(symbol (str "cap-" unit "u-x-top"))
+     (def ~(symbol (str "cap-" name "u-x-top"))
        ~(* unit cap-1u-x-top))
-     (def ~(symbol (str "cap-" unit "u-x-bottom"))
+     (def ~(symbol (str "cap-" name "u-x-bottom"))
        ~(* unit cap-1u-x-bottom))
-     (def ~(symbol (str "cap-" unit "u-y-top"))
+     (def ~(symbol (str "cap-" name "u-y-top"))
        ~(* unit cap-1u-y-bottom))
-     (def ~(symbol (str "cap-" unit "u-y-bottom"))
+     (def ~(symbol (str "cap-" name "u-y-bottom"))
        ~(* unit cap-1u-y-bottom))))
 
-(def-cap-size 2)
-(def-cap-size 1.5)
+(def-cap-size 2 "2")
+(def-cap-size 1.5 "1point5")
 
 (defn thingi-1
   []
@@ -77,9 +77,18 @@
                (union left right))))
 
 (defn make-dsa
-  ""
-  [& {:keys [top bottom x-angle y-angle depth] :or {x-angle 0 y-angle 0}}]
-  (let [sliver            0.0001
+  "Creates a keycap with a DSA-ish profile.
+  It takes a dictionnary as argument the following keys:
+  - `top`: a dictionary with two keys: `x` & `y` describing the size in mm of the top keycap
+  - `bottom`: same as for top, but for the bottom
+  - `depth`: The depth of the well created on the top of the keycap, the value shouldn't exceed 3, otherwise the well would be too deep
+
+  "
+  [& {:keys [top bottom x-angle y-angle depth height] :or {x-angle 0 y-angle 0}}]
+  (let [;; A magic number to adjust the prong length from the top of the well, i'm lazy to figure out how to compute this rn :/
+        z-offset-magic-nr -1.3 ;; 0.5 works well for a well depth of 1.5
+
+        sliver            0.0001
         make-rounded-cube (fn [x y z radius] (minkowski
                                               (cube (- x (* 2 radius)) (- y (* 2 radius)) (- z sliver))
                                               (cylinder radius sliver)))
@@ -87,60 +96,86 @@
         bottom-plate      (with-fs 0.1 (make-rounded-cube (bottom :x) (bottom :y) 0.01 0.5))
         top-plate         (with-fs 0.1 (->> (make-rounded-cube (top :x) (top :y) 0.01 3)
                                             (rotate [(deg->rad x-angle) (deg->rad y-angle) 0])
-                                            (translate [0 0 cap-z])))
-        dish-maker-y      (->> (with-fn 100 (cylinder (get-dish-radus (top :x) depth) 60))
-                               (rotate [(/ pi 2) 0 0])
-                               (translate [0 0 (- (get-dish-radus (top :x) depth) depth)])
-                               (rotate [(deg->rad x-angle) (deg->rad y-angle) 0])
-                               (translate [0 0 cap-z]))
-        dish-maker-x      (->> (with-fn 100 (cylinder (get-dish-radus (top :x) depth) 60))
-                               (rotate [(/ pi 2) 0 (/ pi 2)])
-                               (translate [0 0 (- (get-dish-radus (top :x) depth) depth)])
-                               (rotate [(deg->rad x-angle) (deg->rad y-angle) 0])
-                               (translate [0 0 cap-z]))
+                                            (translate [0 0 height])))
         dish-maker-sphere (->> (with-fn 100 (sphere (get-dish-radus (* (top :x) (Math/sqrt 2)) depth)))
                                (scale [1 2.1 1])
                                (translate [0 0 (- (get-dish-radus (* (top :x) (Math/sqrt 2)) depth) depth)])
                                (rotate [(deg->rad x-angle) (deg->rad y-angle) 0])
-                               (translate [0 0 cap-z]))
+                               (translate [0 0 height]))
         cap               (difference (hull top-plate bottom-plate) dish-maker-sphere)
         bottom-plate-inner (with-fs 0.1 (->> (make-rounded-cube (- (bottom :x) cap-thickness) (- (bottom :y) cap-thickness) 0.01 0.5)
-                                             (translate [0 0 -1])))
+                                             (translate [0 0 -0.001])))
         top-plate-inner (with-fs 0.1
                           (->> (make-rounded-cube (- (top :x) cap-thickness) (- (top :y) cap-thickness) 0.01 3)
-                               (translate [0 0 (- cap-z cap-thickness)])
+                               (translate [0 0 (- height cap-thickness depth)])
                                (rotate [(deg->rad x-angle) (deg->rad y-angle) 0])))
-        cap-inner (difference (hull top-plate-inner bottom-plate-inner) (translate [0 0 (- cap-thickness)] dish-maker-sphere))
-        left-prong (let [x-offset 0
-                         top-plate (->> (cube (get prong-dimensions 0) (get prong-dimensions 1) 0.01)
-                                                        (translate [x-offset 0 5]))
+        cap-inner (-# (hull top-plate-inner bottom-plate-inner))
+        left-prong (let [top-plate (->> (cube (get prong-dimensions 0) (get prong-dimensions 1) 0.01)
+                                        (translate [0 0 5]))
                          bottom-plate (->> (cube (get prong-dimensions 0) (get prong-dimensions 1) 0.01)
-                                           (translate [x-offset 0 cap-z])
-                                           (translate [0 0 -3])
+                                           (translate [0 0 0])
+                                           ;; (translate [0 0 -3])
+                                           ;; ( translate [0 0 (- (- (- (get prong-dimensions 2) z-offset-magic-nr)) (/ height 2))])
                                            (translate [0 0 (- (get prong-dimensions 2))]))]
                      (color [0 1 0] (hull top-plate bottom-plate)))
+        test (+ 1 3)
         right-prong (let [x-offset distance-between-prong-centers
-                         top-plate (->> (cube (get prong-dimensions 0) (get prong-dimensions 1) 0.01)
-                                        (translate [x-offset 0 5]))
-                         bottom-plate (->> (cube (get prong-dimensions 0) (get prong-dimensions 1) 0.01)
-                                           (translate [x-offset 0 cap-z])
-                                           (translate [0 0 -3])
-                                           (translate [0 0 (- (get prong-dimensions 2))]))]
+                          top-plate (->> (cube (get prong-dimensions 0) (get prong-dimensions 1) 0.01)
+                                         (translate [x-offset 0 5]))
+                          bottom-plate (->> (cube (get prong-dimensions 0) (get prong-dimensions 1) 0.01)
+                                            (translate [x-offset 0 0])
+                                           ;; (translate [0 0 -3])
+                                            (translate [0 0 (- (get prong-dimensions 2))]))]
                       (color [0 1 0] (hull top-plate bottom-plate)))
-        prongs (difference (translate [(- (/ distance-between-prong-centers 2)) 0 0] (union left-prong right-prong))dish-maker-sphere )]
+        prongs (difference (translate [(- (/ distance-between-prong-centers 2)) 0 0] (union left-prong right-prong)) dish-maker-sphere)]
     (union prongs (difference cap cap-inner))))
 
-(def spit-scad
-  (spit "resources/out.scad" (write-scad (union
-                                          (make-dsa :top {:x cap-1u-x-top :y cap-2u-y-top}
-                                                    :bottom {:x cap-1u-x-bottom :y cap-2u-y-bottom}
-                                                    :y-angle 10
-                                                    :depth 1.5)
-                                          ;; place-prongs
-                                          ))))
+(defn spit-scad [name obj]
+  (spit (str "resources/" name ".scad") (write-scad obj)))
 
-spit-scad
+(def thumb-2u-hi (make-dsa :top {:x cap-1u-x-top :y cap-2u-y-top}
+                           :bottom {:x cap-1u-x-bottom :y cap-2u-y-bottom}
+                           :y-angle 15
+                           :depth 2
+                           :height (+ 2 cap-z)))
 
+(def thumb-2u-lo (make-dsa :top {:x cap-1u-x-top :y cap-2u-y-top}
+                           :bottom {:x cap-1u-x-bottom :y cap-2u-y-bottom}
+                           :y-angle 10
+                           :height cap-z
+                           :depth 2))
+
+(def thumb-1u-hi (make-dsa :top {:x cap-1u-x-top :y cap-1u-y-top}
+                           :bottom {:x cap-1u-x-bottom :y cap-1u-y-bottom}
+                           :y-angle 15
+                           :depth 2
+                           :height (+ 2 cap-z)))
+
+(def thumb-1point5u (make-dsa :top {:x cap-1point5u-x-top :y cap-1u-y-top}
+                              :bottom {:x cap-1point5u-x-bottom :y cap-1u-y-bottom}
+                              :y-angle 0
+                              :x-angle 5
+                              :depth 1.5
+                              :height cap-z))
+
+(def outter-1point5u (make-dsa :top {:x cap-1point5u-x-top :y cap-1u-y-top}
+                               :bottom {:x cap-1point5u-x-bottom :y cap-1u-y-bottom}
+                               :y-angle -5
+                               :x-angle 0
+                               :depth 1.5
+                               :height (+ 1 cap-z)))
+(def oneU (make-dsa :top {:x cap-1u-x-top :y cap-1u-y-top}
+                               :bottom {:x cap-1u-x-bottom :y cap-1u-y-bottom}
+                               :y-angle 0
+                               :x-angle 0
+                               :depth 1.5
+                               :height cap-z))
+
+;; (spit-scad "out" (union (translate [-25 0 0] oneU) outter-1point5u))
+(spit-scad "outer-1point5u") outter-1point5u
+;; (spit-scad "2u-thumb-1" thumb-2u-1)
+
+;; (spit-scad "2u-thumb-2" thumb-2u-2)
 ;;;;;;;;;;;;;;;;;
 ;; Experiments ;;
 ;;;;;;;;;;;;;;;;;
